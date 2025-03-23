22e4543d8832a265517612e7da9dbae9a11e972f
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-03-23 14:56:50 +0530

    setup pypy package

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='quantize_specific',
+    version='0.0.1',
+    description='a python library that lets you quantize any layer in the model',
+    url='https://github.com/Sh0g0-1758/quantize_specific',
+    author='Shourya Goel',
+    author_email='shouryagoel10000@gmail.com',
+    license='Apache-2.0',
+    packages=['quantize_specific'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
